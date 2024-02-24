0c9d54eb8c746469c64f7c6498b2db838c14ff59
Ak <anieesh.saravanan@gmail.com>
2024-02-24 14:18:54 -0500

    Proper coding practices

--- app.py
+++ app.py
@@ -1,46 +1,56 @@
 import cv2
 import mediapipe as mp
 
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_pose = mp.solutions.pose
+capture = cv2.VideoCapture(0)
+width, height = capture.get(cv2.CAP_PROP_FRAME_WIDTH), capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
+mp_drawing, mp_drawing_styles, mp_pose = mp.solutions.drawing_utils, mp.solutions.drawing_styles, mp.solutions.pose
 
-cap = cv2.VideoCapture(0)
+def remap(value):
 
-with mp_pose.Pose( min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
-    while cap.isOpened():
+    return 1 - value
 
-        success, img = cap.read()
+with mp_pose.Pose(min_detection_confidence = 0.5, min_tracking_confidence = 0.5) as pose:
+
+    while capture.isOpened():
+
+        read, img = capture.read()
         
-        if not success:
-            print("dropped a frame")
+        if not read:
+
+            print("Dropped a Frame")
             continue
-        
+
+        # Temporarily mark the image as not writeable to improve performance when passing to mediapipe
+
         img.flags.writeable = False
+
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         processed = pose.process(img)
-     
-        
-        img.flags.writeable = True #annotating
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        mp_drawing.draw_landmarks(
-            img,
-            processed.pose_landmarks,
-            mp_pose.POSE_CONNECTIONS,
-            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
-        )        
-        
 
-        img_height, img_width, _ = img.shape
-        x = int(processed.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x) * img_width
-        y = int(processed.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y) * img_height
-        print(x,y)
-            
-        
-        
+        img.flags.writeable = True
+
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+        if processed.pose_landmarks:
+
+            mp_drawing.draw_landmarks(
+
+                img,
+                processed.pose_landmarks,
+                mp_pose.POSE_CONNECTIONS,
+                landmark_drawing_spec = mp_drawing_styles.get_default_pose_landmarks_style()
+
+            )
+
+            x = remap(processed.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x) * width
+            y = remap(processed.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y) * height
+            print(f"Nose: ({x}, {y})")
+
         cv2.imshow("pose", cv2.flip(img, 1)) #flip video
+
         if cv2.waitKey(5) & 0xFF == 27:
+
             break
 
-cap.release()
+capture.release()
